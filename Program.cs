0ea83fa61df75d188a1172c5ec5e82ba2bbf5e06
alessandrini11 <alexandreschuame@gmail.com>
2022-04-27 17:10:03 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace wordUnscrambler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("************************************** Welcome to the word unscrambler **************************************");
+            bool next = true;
+            do
+            {
+                Console.WriteLine("enter the team name from 5 great championship in europe to unscrambled and seperate them with a coma(;)");
+                string words = Console.ReadLine();
+                string[] arrayWords = words.Split(';');
+                var unscrambledWordsList = Words.MatchedWords(arrayWords);
+                foreach(string item in unscrambledWordsList)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine("Would you like to continue ?? press (Y) for yes or (N) for No");
+                string decision = Console.ReadLine();
+
+                switch (decision)
+                {
+                    case "y":
+                        next = true;
+                        break;
+                    case "n":
+                        next = false;
+                        break;
+                    default:
+                        break;
+                }
+            } while (next == true);
+        }
+    }
+}
---
+++ Words.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace wordUnscrambler
+{
+    internal class Words
+    {
+        public static List<String> MatchedWords(Array enteredWords)
+        {
+            var unscrambledWords = new List<String>();
+            string[] fileContent = File.ReadAllLines("words.txt");
+            //string[] fileContent = new string[3] { "rome", "beef", "land" };
+            foreach (string line in fileContent)
+            {
+                foreach (string word in enteredWords)
+                {
+                    if(line.Equals(word, StringComparison.OrdinalIgnoreCase))
+                    {
+                        unscrambledWords.Add(line);
+                    } else
+                    {
+                        Char[] wordArray = word.ToCharArray();
+                        Char[] lineArray = line.ToCharArray();
+
+                        Array.Sort(wordArray);
+                        Array.Sort(lineArray);
+
+                        string wordArrayString = new string(wordArray);
+                        string lineArrayString = new string(lineArray);
+
+                        if(wordArrayString.Equals(lineArrayString, StringComparison.OrdinalIgnoreCase))
+                        {
+                            unscrambledWords.Add(line);
+                        }
+                    }
+                }
+            }
+            return unscrambledWords;
+        }
+    }
+}
